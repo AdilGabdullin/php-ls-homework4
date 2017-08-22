00ac8e638a89fa318b6cca7d3d675e72f583b657
AdilGabdullin <Izenbard@gmail.com>
2017-08-22 19:25:04 +0600

    Issue #4. Добавил проверку имени файла

--- php/deleteimage.php
+++ php/deleteimage.php
@@ -6,6 +6,11 @@ if ($_SESSION['access'] !== 'granted') {
 require 'config.php';
 $dbh = new PDO(DSN, DB_USER, DB_PASSWORD);
 $toDelete = $_GET['delete'];
+
+if (!preg_match('/\d+\.(bmp|gif|jpg|png|svg)/', $toDelete)) {
+    echo 'Ошибка. Неверное имя файла';
+    die;
+}
 //Удаляем файл картинки
 $query = "UPDATE users SET photo=NULL WHERE photo=?";
 $dbh->prepare($query)->execute([$toDelete]);
